ac0f56d4b80865e36546d28871534f4f7083d601
Ruslan <ruslan.fekhretdinov@gmail.com>
2016-01-27 22:52:48 +0300

    add_to_cart function

--- public/app.js
+++ public/app.js
@@ -8,3 +8,12 @@ function something()
     alert(x);
 }
 
+function add_to_cart(id)
+{
+    var key = 'product_' + id;
+
+    var x = window.localStorage.getItem(key);
+    x = x * 1 + 1;
+
+    window.localStorage.setItem(key, x);
+}
