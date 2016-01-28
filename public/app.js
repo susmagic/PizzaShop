0c9f7083071ff81857b33eafe37ace9cd55338db
Ruslan <ruslan.fekhretdinov@gmail.com>
2016-01-28 22:46:32 +0300

    update_orders_input

--- public/app.js
+++ public/app.js
@@ -18,7 +18,31 @@ function add_to_cart(id)
     window.localStorage.setItem(key, x);
 
     //вывод кол-во товара в корзине
-    alert('Items in your cart: ' + cart_get_number_of_items());
+    update_orders_input();
+}
+
+function cart_get_number_of_items()
+{
+    var cmt = 0;
+
+    for(var i = 0; i < window.localStorage.length; i++)
+    {
+        var key = window.localStorage.key(i); //получаем ключ
+        var value = window.localStorage.getItem(key); //получаем значение
+
+        if(key.indexOf('product_') == 0)
+        {
+            cnt = cnt + value * 1;
+        }
+    }
+    return orders;
+}
+
+function update_orders_input()
+{
+    var orders = cart_get_orders();
+    $('#orders_input').val(orders);
+
 }
 
 function cart_get_orders()
